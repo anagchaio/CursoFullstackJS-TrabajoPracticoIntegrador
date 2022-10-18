37271cba6de3f4480d8319b1317a4c4a4a0bc830
Ana Gonzalez <agonzalez@ms.local>
2022-10-18 18:51:10 -0300

    files

---
+++ footer.js
@@ -0,0 +1,29 @@
+document.write ('\
+<footer id="footer" class="container-fluid">\
+    <div class="row justify-content-center">\
+      <div class="col"></div>\
+      <div class="col">\
+        <a href="#">Preguntas<br> frecuentes</a>\
+      </div>\
+      <div class="col">\
+        <a href="#">Contáctanos</a>\
+      </div>\
+      <div class="col">\
+        <a href="#">Prensa</a>\
+      </div>\
+      <div class="col">\
+        <a href="#">Conferencias</a>\
+      </div>\
+      <div class="col">\
+        <a href="#">Términos y<br> condiciones</a>\
+      </div>\
+      <div class="col">\
+        <a href="#">Privacidad</a>\
+      </div>\
+      <div class="col">\
+        <a href="#">Estudiantes</a>\
+      </div>\
+      <div class="col"></div>\
+    </div>\
+  </footer>\
+');
---
+++ header.js
@@ -0,0 +1,32 @@
+document.write ('\
+<header id="header">\
+  <ul class="navbar-nav navbar-expand-lg text-light bg-dark"> \
+    <li class="nav-item ml-lg-5 my-auto"> \
+      <a class="nav-item ml-lg-5 my-auto" href="https://www.buenosaires.gob.ar/educacion/codo-codo" target="_blank"> \
+        <image class="nav-item ml-lg-5 my-auto" id="img_logo" src="./images/codoacodo.png" alt="Codo a Codo logo"></image> \
+      </a> \
+    </li> \
+    <li class="nav-item  my-auto mx-lg-2">\
+      <a href="#" class="nav-link h3 text-white">Conf Bs As</a>\
+    </li> \
+    <li class="nav-item  my-auto mx-auto"></li>\
+    </li>\
+    <li class="nav-item  my-auto mx-lg-2">\
+      <a href="#" class="nav-link">La conferencia</a>\
+    </li>\
+    <li class="nav-item  my-auto mx-lg-2">\
+      <a href="#" class="nav-link">Los oradores</a>\
+    </li>\
+    <li class="nav-item  my-auto mx-lg-2">\
+      <a href="#" class="nav-link">El lugar y la fecha</a>\
+    </li>\
+    <li class="nav-item  my-auto mx-lg-2">\
+      <a href="#" class="nav-link">Conviértete en orador</a>\
+    </li>\
+    <li class="nav-item  my-auto mx-lg-2 mr-lg-5">\
+      <a href="#" id="link_comprarTickets" class="nav-link">Comprar Tickets</a>\
+    </li>\
+  </ul>\
+</header>\
+');
+    
